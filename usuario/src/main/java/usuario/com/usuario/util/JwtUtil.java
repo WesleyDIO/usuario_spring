31b38159c2cd267acab50a4f34ddfd99f29c5042
Wesley Diorrani Ferreira <wesleydf@weg.net>
2024-03-13 14:13:51 -0300

    13/03/2024 14:13

--- usuario/src/main/java/usuario/com/usuario/util/JwtUtil.java
+++ usuario/src/main/java/usuario/com/usuario/util/JwtUtil.java
@@ -1,7 +1,7 @@
 package usuario.com.usuario.util;
 
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,29 +13,15 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private SecretKey key;
-    public JwtUtil(){
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        String senha = encoder.encode("senha123");
-        this.key = Keys.hmacShaKeyFor(senha.getBytes(StandardCharsets.UTF_8));
-    }
 
     public String gerarToken(UserDetails userDetails){
-       return Jwts.builder().issuer("WEG").issuedAt(new Date()).expiration(new Date(new Date().getTime() + 300000))
-                .signWith(this.key, Jwts.SIG.HS384).encryptWith(this.key, Jwts.ENC.A128CBC_HS256).subject(userDetails.getUsername()).compact();
+        Algorithm algorithm = Algorithm.HMAC256("senha123");
+       return JWT.create().withIssuer("WEG").withIssuedAt(new Date()).withExpiresAt(new Date(new Date().getTime() + 300000))
+                .withSubject(userDetails.getUsername()).sign(algorithm);
     }
-
-    private Jws<Claims> validarToken(String token){
-         return getParser().parseSignedClaims(token);
-    }
-
-    private JwtParser getParser(){
-        return Jwts.parser().decryptWith(this.key).build();
-    }
-
 
     public String getUsername(String token){
-     return validarToken(token).getPayload().getSubject();
+     return JWT.decode(token).getSignature();
     }
 
 }
